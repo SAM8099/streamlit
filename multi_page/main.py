224676067f6a520e9a8783e52f9f25f5ad5e4133
SAM8099 <samarthgarg8099@gmail.com>
2024-07-29 21:26:59 +0530

    multi_page app

---
+++ multi_page/main.py
@@ -0,0 +1,9 @@
+import streamlit as st 
+
+st.set_page_config(
+    page_title="Main Page",
+    page_icon=":smile:",
+)
+st.title("Main Page")
+st.write("This is the main page of the app")
+st.sidebar.success('main')
